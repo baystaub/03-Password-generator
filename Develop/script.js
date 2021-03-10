312ca18776145d8564c302dd7c9e5970af8c6d24
Ducky <staubbm@gmail.com>
2021-03-10 14:17:08 -0700

    finshes up js password working perfectly now

--- Develop/script.js
+++ Develop/script.js
@@ -1,52 +1,99 @@
 // Assignment Code
 var genBtn = document.querySelector("#generate");
-var upperCase = "ABCDEFGHIJKLMOPQRSTUVWXYZ";
-var lowerCase = "abcdefghijklmopqrstuvwxyz";
-var special = "!@#$%^&*<>?/";
-var RanNumber = "1234567890";
-var password = "";
-var passwordreset = document.querySelector("#password");
+var confirmCharacter = ''
+var length = ''
+var confirmLowercase = ''
+var confirmNumber = ''
+var confirmUppercase = ''
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+var RanNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var choices = '';
 
-genBtn.addEventListener("click", function(event) {
-    event.preventDefault()
-    passwordreset = "";
-    password = "";
-    var length = 0
-    length = prompt("how long would you like your password from 8-25?")
-    if (length >= 8) {
-        password.length = length;
-        var lowerQ = confirm("would you like lower cases in your password?");
-        console.log(lowerQ)
-        var upperQ = confirm("would you like upper casses in your password?");
-        console.log(upperQ)
-        var specialQ = confirm("would you like special charecters in your password?")
-        console.log(specialQ)
-        var numberQ = confirm("would you like numbers in your password?");
-        console.log(numberQ)
+genBtn.addEventListener("click", generatePassword)
+
+
+
+function generatePassword() {
+    length = parseInt(prompt("How Long would you like your password? please choose between 8 and 50"));
+
+    if (!length) {
+        alert('Please enter a value')
+
+    } else if (length < 8 || length > 50) {
+        length = parseInt(prompt("Must choose between 8 and 50"));
+
     } else {
-        alert("please pick a number within the range!")
+        confirmNumber = confirm('Would you like numbers in this password?')
+        confirmCharacter = confirm('Would you like special charecters in this password?')
+        confirmLowercase = confirm("would you like lowercase letters in this password?")
+        confirmUppercase = confirm("would you like uppercase letters in this password?")
+    };
+
+
+    if (!confirmCharacter && !confirmNumber && !confirmUppercase && !confirmLowercase) {
+        choices = alert("You must choose a criteria!");
+    } else if (confirmCharacter && confirmNumber && confirmUppercase && confirmLowercase) {
+        choices = character.concat(RanNumber, upperCase, lowerCase);
+
+    } else if (confirmCharacter && confirmNumber && confirmUppercase) {
+        choices = character.concat(RanNumber, upperCase);
+
+    } else if (confirmCharacter && confirmNumber && confirmLowercase) {
+        choices = character.concat(RanNumber, lowerCase);
+
+    } else if (confirmCharacter && confirmUppercase && confirmLowercase) {
+        choices = character.concat(upperCase, lowerCase);
+
+    } else if (confirmNumber && confirmUppercase && confirmLowercase) {
+        choices = RanNumber.concat(upperCase, lowerCase);
+
+    } else if (confirmCharacter && confirmNumber) {
+        choices = character.concat(RanNumber);
+
+    } else if (confirmCharacter && confirmUppercase) {
+        choices = character.concat(upperCase);
+
+    } else if (confirmCharacter && confirmLowercase) {
+        choices = character.concat(lowerCase);
+
+    } else if (confirmNumber && confirmUppercase) {
+        choices = RanNumber.concat(upperCase);
+
+    } else if (confirmNumber && confirmLowercase) {
+        choices = RanNumber.concat(lowerCase);
+
+    } else if (confirmUppercase && confirmLowercase) {
+        choices = upperCase.concat(lowerCase);
+
+    } else if (confirmCharacter) {
+        choices = character;
+
+    } else if (confirmNumber) {
+        choices = RanNumber;
+
+    } else if (confirmUppercase) {
+        choices = upperCase;
+
+    } else if (confirmLowercase) {
+        choices = lowerCase;
     }
 
-    if (lowerQ === true); {
-        password = password + lowerCase.charAt(Math.floor(Math.random() * Math.floor(lowerCase.length - 1)));
-        console.log(password)
+
+    var password = [];
+
+    for (var i = 0; i < length; i++) {
+        var picked = choices[Math.floor(Math.random() * choices.length)]
+        password.push(picked)
     }
 
-    if (upperQ === true); {
-        password = password + upperCase.charAt(Math.floor(Math.random() * Math.floor(upperCase.length - 1)));
-        console.log(password)
-    }
+    var pw = password.join("");
+    Input(pw);
+    return pw;
+}
 
-    if (specialQ === true); {
-        password = password + special.charAt(Math.floor(Math.random() * Math.floor(special.length - 1)));
-        console.log(password)
-    }
-
-    if (upperQ === true); {
-        password = password + RanNumber.charAt(Math.floor(Math.random() * Math.floor(RanNumber.length - 1)));
-        console.log(password)
-    }
-
-
-    passwordreset = password
-})
+function Input(pw) {
+    document.getElementById("password").textContent = pw;
+    console.log(pw)
+}
